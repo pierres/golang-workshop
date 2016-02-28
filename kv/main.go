649288e7e13e71bff4ba841a7be03800bff456b6
Pierre Schmitz <pierre@archlinux.de>
2016-02-28 20:08:58 +0100

    Implement kv as HTTP API

--- kv/main.go
+++ kv/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
 func main() {
-	args := Input(os.Args[1:])
-
 	storageFileName := os.TempDir() + "/kv"
 	file, err := os.Open(storageFileName)
 
@@ -18,16 +18,20 @@ func main() {
 	}
 	file.Close()
 
-	if args.IsWriteStatement() {
-		k.Merge(args.Map())
-
-		file, _ := os.Create(storageFileName)
-		defer file.Close()
-
-		k.Write(file)
-	} else {
-		requestedKeys := args.Request()
-
-		fmt.Print(k.Filter(requestedKeys))
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			key := r.URL.Path[1:]
+			fmt.Fprint(w, k.Filter([]string{key}))
+		} else if r.Method == "POST" {
+			key := r.URL.Path[1:]
+			value, _ := ioutil.ReadAll(r.Body)
+			k.Merge(Data{key: string(value)})
+			file, _ := os.Create(storageFileName)
+			defer file.Close()
+			k.Write(file)
+		} else {
+			http.Error(w, "Method not supported", http.StatusInternalServerError)
+		}
+	})
+	http.ListenAndServe(":8080", nil)
 }
