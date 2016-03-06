453c82fba68f68d75ac8b91d04de03680df82ac8
Pierre Schmitz <pierre@archlinux.de>
2016-03-06 16:24:48 +0100

    Implement Handler interface

--- kv/main.go
+++ kv/main.go
@@ -7,31 +7,43 @@ import (
 	"os"
 )
 
-func main() {
-	storageFileName := os.TempDir() + "/kv"
-	file, err := os.Open(storageFileName)
+type Handler struct {
+	storageFileName string
+	k               Store
+}
 
-	k := NewStore(Data{})
+func NewHandler(storageFileName string) Handler {
+	h := Handler{}
 
+	h.storageFileName = storageFileName
+	file, err := os.Open(h.storageFileName)
+	h.k = NewStore(Data{})
 	if err == nil {
-		k.Read(file)
+		h.k.Read(file)
 	}
 	file.Close()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			key := r.URL.Path[1:]
-			fmt.Fprint(w, k.Filter([]string{key}))
-		} else if r.Method == "POST" {
-			key := r.URL.Path[1:]
-			value, _ := ioutil.ReadAll(r.Body)
-			k.Merge(Data{key: string(value)})
-			file, _ := os.Create(storageFileName)
-			defer file.Close()
-			k.Write(file)
-		} else {
-			http.Error(w, "Method not supported", http.StatusInternalServerError)
-		}
-	})
+	return h
+}
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		key := r.URL.Path[1:]
+		fmt.Fprint(w, h.k.Filter([]string{key}))
+	} else if r.Method == "POST" {
+		key := r.URL.Path[1:]
+		value, _ := ioutil.ReadAll(r.Body)
+		h.k.Merge(Data{key: string(value)})
+		file, _ := os.Create(h.storageFileName)
+		defer file.Close()
+		h.k.Write(file)
+	} else {
+		http.Error(w, "Method not supported", http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	handler := NewHandler(os.TempDir() + "/kv")
+	http.Handle("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
